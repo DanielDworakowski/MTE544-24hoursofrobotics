855fb04aec4a7326ea2ed0037a1b21134d381574
Daniel Dworakowski <daniel@Daniels-MacBook-Pro.local>
2017-12-16 16:02:50 -0500

    working

--- Q1-droneracing/mappingbres.m
+++ Q1-droneracing/mappingbres.m
@@ -3,7 +3,9 @@ clear; clc;
 
 % Simulation time
 Tmax = 9999;
-T = 0:Tmax;
+dt = 1/5; 
+
+T = 0:Tmax / dt;
 
 % Initial Robot location
 % x0 = [5 20 0]
@@ -12,7 +14,7 @@ u = [3 0 -3 0;
      0 3 0 -3];
  ui=1;
 % Robot sensor rotation command
-w = 0.3*ones(length(T));
+% w = 0.3*ones(length(T));
 
 run('racing.m')
 % map = map.';
@@ -34,20 +36,46 @@ beta = 0.05; % Width of a beam (Angle beyond which to exclude)
 %State Initialization
 x = zeros(3,length(T)+1);
 x(:,1) = x0;
-dt = 1/5; 
 
 goal0 = [440 642];
+goal = goal0;
 unseen = ones(size(map));
 frontier = zeros(size(map));
 
-
+spincnt = 0
+nspins = 0 
+gain = 10
+minDist = 10
 %% Main simulation
 for t=2:length(T)
     % Robot motion
-    x(:,t) =  trajRoll(x(:,t - 1), m, goal0, unseen,frontier);
+    [x(:,t), spincnt, nspins] =  trajRoll(x(:,t - 1), m, goal, unseen,frontier,spincnt, nspins, gain);
     % Generate a measurement data set
     meas_r = getranges(map, x(:,t),meas_phi,rmax, rmin);
+    disttoGoal = norm(goal - x(1:2,t).');
+    
+%     MODIFIERS TO BIAS WHERE TO GO.
+    if disttoGoal < minDist
 
+      goal = startpos;
+      startpos = startpos * 10;
+      gain = 5;
+      if minDist == 10
+      minDist = 300;
+      else
+        minDist = 10
+        gain = 0
+%         goal = [250, 350]
+      end
+      
+    end
+    t
+    if t == 30 
+    spincnt = 15
+    end
+%     if mod(t,60) == 0
+%       spincnt = 15;
+%     end
 
     %% Map update
     measL = zeros(M,N);
@@ -90,7 +118,8 @@ for t=2:length(T)
     
     % Map and vehicle path
     figure(1);clf; hold on;
-    image(100*(1-map));
+%     image(100*(1-map));
+image(100*frontier);
     colormap(gray);
     plot(x(2,1:t),x(1,1:t),'bx-')
     axis([0 N 0 M])
@@ -126,13 +155,16 @@ function [x_plus] = motModel(x, v, theta, dt)
   x_plus = x; 
   x_plus(1) = x_plus(1) + v * cos(x(3)) * dt; 
   x_plus(2) = x_plus(2) + v * sin(x(3)) * dt; 
+%   x_plus(3) = x_plus(3) + angleWrap(theta) * dt;
   x_plus(3) = angleWrap(theta);
 end
 
-function [x_new] = trajRoll(x_cur, map, xF, unseen, frontier)
+function [x_new, spincnt, nspins] = trajRoll(x_cur, map, xF, unseen, frontier, spincnt, nspins, gain)
   dt = 1/5;
-  uMin = [0.3 -2 + x_cur(3)]; % bounds on inputs, [velocity, rotation rate]
-  uMax = [0.3 2 + x_cur(3)]; % bounds on inputs, [velocity, rotation rate]
+  uMin = [0.8 -1.8 + x_cur(3)]; % bounds on inputs, [velocity, rotation rate]
+  uMax = [0.8 1.8 + x_cur(3)]; % bounds on inputs, [velocity, rotation rate]
+%   uMin = [1. -0.3];
+%   uMax = [1. 0.3];
   uR = uMax-uMin; % range of inputs
   sMin = 10; % steps to move
   sMax = 30; % steps to compute for rollout
@@ -141,6 +173,7 @@ function [x_new] = trajRoll(x_cur, map, xF, unseen, frontier)
   x_new = x_cur;
   checkLength = 5;
   [frontRows, frontCols] = find(frontier);
+  [N M] =  size(map);
   for i = 1:n_traj
       % Constant speed, linear distribution of turn rates
       input = [uR(1)/2+uMin(1) uR(2)*(i-1)/(n_traj-1)+uMin(2)];
@@ -148,10 +181,18 @@ function [x_new] = trajRoll(x_cur, map, xF, unseen, frontier)
 
       % Propagate Dynamics
       x = x_cur;
+      invalid = 0;
       for j=2:steps
 %           x(:, j) = x(:,j-1)+[input(1)*cos(x(3,j-1))*dt; input(1)*sin(x(3,j-1))*dt; input(2)*dt]
           x(:, j) = motModel(x(:, j-1), input(1), input(2), dt);
+          if (x(2,j) <=1||x(2,j)>=M||x(1,j)<=1||x(1,j)>=N)
+            invalid = 1;
+          end
       end
+      if invalid
+        continue
+      end
+        
       idx = sub2ind(size(map), uint32(x(1,:)), uint32(x(2,:)));
       keep = map(idx) < 0.4;
       if (sum(keep)==steps)
@@ -160,7 +201,13 @@ function [x_new] = trajRoll(x_cur, map, xF, unseen, frontier)
         iY = x(2,end);
         dist = 0;
         if ~isempty(frontRows)
-          dist = min(norm([frontRows - iY, frontCols - iX]))
+%           [frontRows - iY, frontCols - iX]
+% norm([frontRows - iY, frontCols - iX])
+%           norm([frontRows - iX, frontCols - iY],1);
+          dist = min(sqrt(sum([frontRows - iX, frontCols - iY].^2,2)))
+          if dist > 200
+          dist = 0
+          end
         end
          
         
@@ -168,9 +215,11 @@ function [x_new] = trajRoll(x_cur, map, xF, unseen, frontier)
 %         unseenScore = unseen(x(1, end), x(2, end))
         plot(x(2,:),x(1,:),'g');
         % Score the trajectory
-        togo_cur = norm(x(end,1:2)-xF) - 0.1*dist;
+%         thetaWeight = 
 
-        score_cur = togo_cur ; %- 0.1*obs_dist;
+        togo_cur = norm(x(1:2,end).'-xF) + gain * dist;
+
+        score_cur = togo_cur %- 0.1*obs_dist;
         if (score_cur < score_step)
             score_step = score_cur;
 
@@ -182,12 +231,21 @@ function [x_new] = trajRoll(x_cur, map, xF, unseen, frontier)
       end
   end
   % Check if no progress is made
-  
-  if (x_new==x_cur)
-      x_new(3)=x_new(3)-0.1;
+  if (all(x_new==x_cur) || ((spincnt > 0) && nspins < 1))
+      x_new = x_cur;
+      x_new(3)=x_new(3)-0.4;
+      spincnt = spincnt - 1
+      if spincnt < 0
+        spincnt = 15;
+      end
+      if spincnt == 1
+        nspins = nspins + 1;
+      end
+      display('here')
   else
       plot(x_plot(2,:),x_plot(1,:),'b');
       plot(x_plot(2,end),x_plot(1,end),'bo');
+      nspins = 0;
   end
   drawnow;
 end
