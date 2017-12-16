7f057df7d6e780ce06c0b1fbf12ee1aaa7f4f535
Daniel Dworakowski <daniel@Daniels-MacBook-Pro.local>
2017-12-16 16:03:11 -0500

    q2

---
+++ Q2-planning/planningEnv.m
@@ -0,0 +1,19 @@
+% Set up environment
+rng(1)
+posMinBound = [0 0];
+posMaxBound = [50 40];
+numObsts = 70;
+endPos = [1 1];
+startPos = [48.5 38.5];
+
+minLen.a = 1;
+maxLen.a = 3;
+minLen.b = 2;
+maxLen.b = 6;
+
+obstBuffer = 0.5;
+maxCount = 10000;
+
+[aObsts,bObsts,obsPtsStore] = polygonal_world(posMinBound, posMaxBound, minLen, maxLen, numObsts, startPos, endPos, obstBuffer, maxCount);
+
+plotEnvironment(obsPtsStore,posMinBound, posMaxBound, startPos, endPos, 1);
