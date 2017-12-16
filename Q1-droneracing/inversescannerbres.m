2eff3ec630f3fb34f47ccdf7d46ab11a20ad1a36
Daniel Dworakowski <daniel@Daniels-MacBook-Pro.local>
2017-12-16 11:43:28 -0500

    more or less working motion and trajectory rolout

--- Q1-droneracing/inversescannerbres.m
+++ Q1-droneracing/inversescannerbres.m
@@ -12,12 +12,12 @@ endpt = [x y] + r*[cos(theta) sin(theta)];
 x2 = max(1,min(M,round(endpt(1))));
 y2 = max(1,min(N,round(endpt(2))));
 
-[list(:,1) list(:,2)] = bresenham(x1,y1,x2,y2);
-m = [list 0.4*ones(length(list),1)];
-if (r<rmax)
-    m(end,3) = 0.6;
-end
 if (r < 0)
   m = [];
+else
+  [list(:,1) list(:,2)] = bresenham(x1,y1,x2,y2);
+  m = [list 0.4*ones(length(list),1)];
+  if (r<rmax)
+    m(end,3) = 0.6;
+  end
 end
-
